656da5ee90328ccd4b3f9efe8e72e5fbd2dadfd0
Anthony Spault <anthony@spault.fr>
2017-05-15 15:32:03 +0200

    Start integrating psd template to css

---
+++ assets/js/home.js
@@ -0,0 +1,6 @@
+$(function() {
+    $(".rslides").responsiveSlides({
+        random: true,          // Boolean: Randomize the order of the slides, true or false
+        pause: true,           // Boolean: Pause on hover, true or false
+    });
+});
---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#burger-toggle').click(function(){
+        $(this).toggleClass('active');
+        $('#mobile-nav').toggleClass('latAparece');
+        $('#mobile-nav').toggleClass('latDesaparece');
+        return false;
+    });
+});
